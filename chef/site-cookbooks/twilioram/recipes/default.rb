05da4ca41d86085fc02ad4840688eacf635ac5af
Andy Fraley <andrew.fraley@gmail.com>
2014-12-31 01:22:33 -0500

    added sinatra-reloader to chef recipe

--- chef/site-cookbooks/twilioram/recipes/default.rb
+++ chef/site-cookbooks/twilioram/recipes/default.rb
@@ -1,15 +1,17 @@
 package 'build-essential'
 package 'ruby'
 package 'ruby-dev'
+package 'git'
 gem_package "sinatra"
+gem_package "sinatra-reloader"
 gem_package "wolfram-alpha"
 gem_package "twilio-ruby"
 
-application "twilioram" do
-  path "/opt/twilioram/source"
-  action :none
-
+application 'twilioram' do
+  path '/opt/twilioram'
+  repository 'https://github.com/andrewfraley/twilioram.git'
   nginx_load_balancer do
-    hosts ['foo.bar.com']
+    hosts ['localhost']
+    application_port 4567
   end
 end
